707e8d34a5ac2214db4a0bcd2e1e987883df3293
Rehbar Khan <rehbar@eng.rizvi.edu.in>
2025-11-03 18:30:15 +0530

    Update Events-component.jsx

--- client/src/components/Events-component.jsx
+++ client/src/components/Events-component.jsx
@@ -122,13 +122,13 @@ const handleClick = async () => {
         </div>
       ) : (
         <>
-          <button
+          {/* <button
             onClick={handleClick}
             className="relative inline-flex items-center justify-center px-8 py-3 overflow-hidden font-semibold text-black transition-all duration-300 bg-neutral-300  rounded-xl hover:from-neutral-700 hover:to-neutral-600 group"
           >
             <span className="absolute inset-0 w-full h-full transition-all duration-300 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 opacity-0 group-hover:opacity-100 blur-lg"></span>
             <span className="relative z-10">Give Test</span>
-          </button>
+          </button> */}
 
           <br />
 
