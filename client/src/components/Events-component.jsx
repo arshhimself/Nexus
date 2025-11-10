15f77023072b96feb89f0f920f4d4ef082494ae8
Rehbar Khan <rehbar@eng.rizvi.edu.in>
2025-11-10 14:21:32 +0530

    Update Events-component.jsx

--- client/src/components/Events-component.jsx
+++ client/src/components/Events-component.jsx
@@ -142,13 +142,11 @@ const handleClick = async () => {
             <span className="relative z-10">View LeaderBoard</span>
           </button>
           <button
-            className="relative m-5 inline-flex items-center justify-center px-8 py-3 overflow-hidden font-semibold text-black transition-all duration-300 bg-neutral-300 rounded-xl hover:from-neutral-700 hover:to-neutral-600 group"
-            onClick={() => {
-              router.push("/feedback");
-            }}
+            onClick={handleClick}
+            className="relative inline-flex items-center justify-center px-8 py-3 overflow-hidden font-semibold text-black transition-all duration-300 bg-neutral-300  rounded-xl hover:from-neutral-700 hover:to-neutral-600 group"
           >
             <span className="absolute inset-0 w-full h-full transition-all duration-300 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 opacity-0 group-hover:opacity-100 blur-lg"></span>
-            <span className="relative z-10">Give Feedback</span>
+            <span className="relative z-10">Give Test</span>
           </button>
         </>
       )}
